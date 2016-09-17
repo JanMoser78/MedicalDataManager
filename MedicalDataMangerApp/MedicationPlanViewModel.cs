f6a36ba2bd60089fd19cb3b3d4cb89b408eda0a5
Jan Moser <jan.moser@gmx.ch>
2016-09-17 04:05:58 +0200

    updated datamodels

---
+++ MedicalDataMangerApp/MedicationPlanViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MedicalDataManagerModel;
+using MedicalDataMangerApp.Annotations;
+
+namespace MedicalDataMangerApp
+{
+   public class MedicationPlanViewModel : INotifyPropertyChanged
+    {
+       public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<MedicationPlan> MedicationPlans { get; private set; }
+
+       public MedicationPlanViewModel()
+       {
+           using (var context = new MedicalDataManagerDataBaseContainer())
+           {
+               this.MedicationPlans = new ObservableCollection<MedicationPlan>(context.MedicationPlans.ToList());
+           }
+       }
+
+       [NotifyPropertyChangedInvocator]
+       protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+       {
+           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+       }
+    }
+}
